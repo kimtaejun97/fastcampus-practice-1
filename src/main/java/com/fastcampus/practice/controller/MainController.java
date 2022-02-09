ea95d66820bb8a6fd4bd9d86f8fc65fb4671559f
kimtaejun97 <kim_taejun@naver.com>
2022-02-09 10:44:48 +0900

    refactor: Lombok 추가
    - 빈으로는 JavaSort 선택

--- src/main/java/com/fastcampus/practice/controller/MainController.java
+++ src/main/java/com/fastcampus/practice/controller/MainController.java
@@ -1,19 +1,21 @@
 package com.fastcampus.practice.controller;
 
 import com.fastcampus.practice.service.StringSortService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class MainController {
 
     private final StringSortService stringSortService;
 
     @GetMapping("/sort")
-        this.stringSortService = stringSortService;
+    public List<String> main(@RequestParam List<String> list) {
     }
 
     public String main(@RequestParam List<String> list) {
